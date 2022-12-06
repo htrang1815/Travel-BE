365b45616c75e3ebaa51416b1171ba175b295adf
htrang1815 <htrang1815@gmail.com>
2022-12-07 00:31:58 +0700

    be

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -46,11 +46,12 @@ exports.getCheckoutSession = catchAsync(async (req, res) => {
 });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email }).id;
+  // const tour = session.client_reference_id;
+  // const user = await User.findOne({ email: session.customer_email }).id;
 
-  const price = session.line_items[0].amount / 100;
-  await Booking.create({ tour, user, price });
+  // const price = session.line_items[0].amount / 100;
+  // await Booking.create({ tour, user, price });
+  console.log(session);
 };
 
 exports.webhookCheckout = catchAsync(async (req, res, next) => {
