761dde1fae6359869e926fa8deb57c7535c1108f
htrang1815 <htrang1815@gmail.com>
2022-12-07 13:50:00 +0700

    be

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -68,11 +68,11 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
   // const reqBuffer = await buffer(req);
   // B2: Tạo event
   let event;
-  console.log({
-    body: req.body,
-    signature,
-    secret: process.env.STRIPE_WEBHOOK_SECRET,
-  });
+  // console.log({
+  //   body: req.body,
+  //   signature,
+  //   secret: process.env.STRIPE_WEBHOOK_SECRET,
+  // });
   try {
     event = await stripeAPI.webhooks.constructEvent(
       req.rawBody,
